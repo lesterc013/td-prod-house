244378e05c54160b1e47eb993d265242bd5ff3f5
mtse-big-best <stetcapdev@gmail.com>
2026-06-19 15:32:23 +0800

    feat: Build process to generate RAG based reply for a query

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
 import { documents, queries } from './hardcodedTests.js';
 
-const EMBEDDING_MODEL = 'nomic-embed-text';
+const EMBEDDING_MODEL_NAME = 'nomic-embed-text';
 const EMBEDDING_ENDPOINT = 'http://localhost:11434/api/embed';
+
+const GENERATE_MODEL_NAME = 'qwen2.5:14b';
+const GENERATE_ENDPOINT = 'http://localhost:11434/api/generate';
+const SYSTEM_PROMPT =
+  "You are a helpful assistant. Answer ONLY using the provided context. If the answer is not in the context, reply with: 'Not found in database.' but also list out the documents that were provided to you as context in the prompt so the user can reference them in case there is something useful for them there.";
 
 async function getEmbedding(input) {
   const res = await fetch(EMBEDDING_ENDPOINT, {
     method: 'POST',
     body: JSON.stringify({
-      model: EMBEDDING_MODEL,
+      model: EMBEDDING_MODEL_NAME,
       input,
     }),
   });
 
   return await res.json();
+}
+
+async function generateReply(query, topDocuments) {
+  // Create the prompt
+  // Call the api with fetch and the message body
+  const res = await fetch(GENERATE_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify({
+      model: GENERATE_MODEL_NAME,
+      system: SYSTEM_PROMPT,
+      prompt: buildPrompt(query, topDocuments),
+    }),
+  });
+
+  // Get the body - which is a pipe actually. A pipe of data.
+  const reader = res.body.getReader();
+  const decoder = new TextDecoder(); // One chunk of bytes at a time
+
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      console.log('\nCompleted reading.');
+      break;
+    }
+
+    // value returned from ReadableStream are bytes which need to be decoded.
+    const resObj = JSON.parse(decoder.decode(value));
+    process.stdout.write(resObj.response);
+  }
 }
 
 function calcCosineSimilarity(a, b) {
@@ -49,31 +83,41 @@ function getMagnitude(vector) {
   return Math.sqrt(totalSquaredSum);
 }
 
-// Storing hardcoded documents' embeddings in embeddings.
-const embeddings = new Map();
-for (const d of documents) {
-  const e = await getEmbedding(d);
-  embeddings.set(d, e);
-  console.log(e.embeddings[0].length);
+function buildPrompt(query, topDocuments) {
+  let contextString = '<context>\n';
+  for (const doc of topDocuments) {
+    contextString += `${doc.document}\n`;
+    contextString += `---\n`;
+  }
+  contextString += '</context>\n';
+
+  // Then add the query
+  let queryString = `Please answer the following question based ONLY on the reference information above. If the answer cannot be found in the context, state exactly: "Not found in database."\n Question: ${query}`;
+
+  return contextString.concat(queryString);
 }
 
+// Storing hardcoded documents' embeddings in embeddings.
+const documentEmbeddings = new Map();
+for (const d of documents) {
+  const e = await getEmbedding(d);
+  documentEmbeddings.set(d, e);
+}
+
+// Hardcoded testing of the query -> answer pipeline
 for (const q of queries) {
   const qEmbedding = await getEmbedding(q);
   const qVector = qEmbedding.embeddings[0];
 
   const results = [];
-  for (const [doc, docResponse] of embeddings) {
+  for (const [doc, docResponse] of documentEmbeddings) {
     const docVector = docResponse.embeddings[0];
     const score = calcCosineSimilarity(qVector, docVector);
     results.push({ document: doc, score });
   }
 
-  results.sort((a, b) => b.score - a.score);
-
-  console.log(`\nQuery: "${q}"`);
-  results.forEach((r, i) => {
-    console.log(
-      `  ${i + 1}. [${r.score.toFixed(4)}] ${r.document.slice(0, 80)}...`,
-    );
-  });
+  const topThree = results.toSorted((a, b) => b.score - a.score).slice(0, 3);
+  console.log(`Generating response for query: ${q}`);
+  await generateReply(q, topThree);
+  console.log();
 }
