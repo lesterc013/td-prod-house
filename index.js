1ec679e7d1a8dc5a4de528de5a28ebecf287837e
mtse-big-best <stetcapdev@gmail.com>
2026-06-19 15:49:50 +0800

    chore: Add TODOs for future refactor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { documents, queries } from './hardcodedTests.js';
 
+// TODO: Consider exporting constants into env? or just a file with constants.
 const EMBEDDING_MODEL_NAME = 'nomic-embed-text';
 const EMBEDDING_ENDPOINT = 'http://localhost:11434/api/embed';
 
@@ -8,6 +9,7 @@ const GENERATE_ENDPOINT = 'http://localhost:11434/api/generate';
 const SYSTEM_PROMPT =
   "You are a helpful assistant. Answer ONLY using the provided context. If the answer is not in the context, reply with: 'Not found in database.' but also list out the documents that were provided to you as context in the prompt so the user can reference them in case there is something useful for them there.";
 
+// TODO: Create modules for all the helper functions
 async function getEmbedding(input) {
   const res = await fetch(EMBEDDING_ENDPOINT, {
     method: 'POST',
